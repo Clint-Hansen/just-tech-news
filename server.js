f77ea7ba8543f1f78e6bf4bd94236a901acc8252
Clint Hansen <clint.hansen2009@gmail.com>
2021-04-20 17:03:15 -0600

    authentication and cookies

--- server.js
+++ server.js
@@ -4,7 +4,20 @@ const hbs = exphbs.create({});
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 
+const session = require('express-session');
+
 const path = require('path');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -14,6 +27,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+app.use(session(sess));
 
 // turn on routes
 app.use(routes);
